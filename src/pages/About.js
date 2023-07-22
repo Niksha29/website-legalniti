33bed1d809db2b34d03cdd2fd25411b55587f92e
Nikhil Sharma <nikhilsharma.is22@bmsce.ac.in>
2023-07-22 17:40:57 +0530

    hover

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header";
-import AboutContent from "./Aboutcontent";
-import PageHeader from "../components/PageHeader";
-import About from "../components/About/home-two";
-import Services from "../components/Services";
-import BrandLogo from "../components/BrandLogo";
+
+import AboutHeader from "../components/About/home-two";
+import About from "../components/About/home-one";
 import Funfact from "../components/Funfact";
 import CallToAction from "../components/CallToAction";
 import Footer from "../components/Footer";
 import LoginRegister from "../components/LoginRegister";
 import MobileMenu from "../components/MobileMenu";
-import Features from "../components/Features";
-import Testimonial from "../components/Testimonials/home-two";
-import Team from "../components/Team/home-two";
+
 
 import ServiceThumb from "../assets/img/about.jpg";
 
@@ -24,13 +20,14 @@ const PageAbout = () => {
       
       {/* <PageHeader/> */}
       
-      <About
+      <AboutHeader
         title={"Our Team"}
         heading="Meet Our <br/> Expert Member"
         thumb={ServiceThumb}
         content="<b>Businex</b> always try to provide the best Business Solutions for Clinets to grow up their Business very sharply and smoothly. We voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
       />
-      <AboutContent />
+      <About />
+      {/* <AboutContent /> */}
       
       {/* <Features classes={"sm-top"} /> */}
       {/* <Testimonial /> */}
